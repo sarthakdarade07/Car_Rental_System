4cd6dd08ac87a615d3780bdd4fa458ab8c5647d5
sarthakdarade07 <sarthakdarade07@gmail.com>
2024-10-18 01:09:26 +0530

    Added basic transport vechile class

--- Car_rental_system.cpp
+++ Car_rental_system.cpp
@@ -34,7 +34,6 @@ company::company()
   Cars.push_back(company("Audi", "Q5", "Available", 125));
   Cars.push_back(company("Honda", "City", "Available", 25));
   Cars.push_back(company("Suzuki", "Swift", "Available", 15));
-  
 }
 
 // Parameterized constructor for setting values
@@ -102,10 +101,61 @@ void company::showCarlist()
   }
   cout << endl;
 }
+class Transport_vehicle
+{
+
+private:
+  string vehicleName, cname;
+  string description;
+  double price, km;
+  vector<string> vec1;
+  vector<string> vec2;
+  vector<double> vec3;
+
+public:
+  Transport_vehicle()
+  {
+    vec1.push_back("Tata MiniTruck");
+    vec2.push_back("Good for small distance and low weight");
+    vec3.push_back(30.0);
+  }
+  void book_vehicle()
+  {
+    cout << "Enter vehical name: ";
+    cin.ignore();
+    getline(cin, vehicleName);
+
+    for (int i = 0; i < vec1.size(); i++)
+    {
+      if (vehicleName == vec1[i])
+      {
+        cout << "Enter your Name: ";
+        cin.ignore();
+        getline(cin, cname);
+        cout << "Enter Km";
+        cin >> km;
+        cout << "Vehicle Booked Succesfully\n";
+      }
+      else
+      {
+        cout << "Invalid Car";
+      }
+      return;
+    }
+  }
+  void display()
+  {
+    for (int i = 0; i < vec1.size(); i++)
+    {
+      cout<<"Vehicle Name  "<<"  Vechile Description"<<"\t\t\t\t  Price \\km \n";
+      cout << vec1[i] << " \t" << vec2[i] << "\t\t\t" << vec3[i] << endl;
+    }
+  }
+};
 
 // Class user to accept user details
 
-class user : protected company
+class user : protected company,protected Transport_vehicle
 {
 private:
   string name, carName, carModle, pickup, destination;
@@ -225,7 +275,7 @@ void user::getchoice_user()
   int ch;
   do
   {
-    cout << "\n 1.Book Car\n 2.Return Car\n 3.Back\n";
+    cout << "\n 1.Book Car\n 2.Return Car\n 3.Book transport vehicle\n 4.back\n";
     cout << "Enter Your choice:";
     cin >> ch;
     switch (ch)
@@ -237,7 +287,11 @@ void user::getchoice_user()
       returnCar();
       break;
     case 3:
-      return;
+      display();
+      book_vehicle();
+      break;
+     case 4:
+     return;
       break;
     default:
       cout << "Entered valid choice\n";
@@ -273,46 +327,48 @@ void user::getchoice_company()
   } while (ch != 4);
 }
 
-int main()
-{
-  user u1;
-  int ch1;
-  do
-  {
-    cout << "\n 1.User\n 2.Company\n 3.Exit\n";
-    cout << "Enter Your choice:";
 
-    try
-    {             // Exception handling : If user enter choice other than int value it will
-      cin >> ch1; // get terminated in controlled manner
-      if (ch1 == 0)
+
+      int main()
       {
-        throw(ch1);
-      }
-      else
-      {
-        switch (ch1)
+        user u1;
+        int ch1;
+        do
         {
-        case 1:
-          u1.getchoice_user();
-          break;
-        case 2:
-          u1.getchoice_company();
-          break;
-        case 3:
-          cout << "THANK YOU!!";
-          break;
-        default:
-          cout << "Enter Valid Choice";
-        }
-      }
-    }
-    catch (int b)
-    {
-      cout << "\nEnter choice in Integer!! \n\n";
-      break;
-    }
-  } while (ch1 != 3);
+          cout << "\n 1.User\n 2.Company\n 3.Exit\n";
+          cout << "Enter Your choice:";
 
-  return 0;
-}
+          try
+          {             // Exception handling : If user enter choice other than int value it will
+            cin >> ch1; // get terminated in controlled manner
+            if (ch1 == 0)
+            {
+              throw(ch1);
+            }
+            else
+            {
+              switch (ch1)
+              {
+              case 1:
+                u1.getchoice_user();
+                break;
+              case 2:
+                u1.getchoice_company();
+                break;
+              case 3:
+                cout << "THANK YOU!!";
+                break;
+              default:
+                cout << "Enter Valid Choice";
+              }
+            }
+          }
+          catch (int b)
+          {
+            cout << "\nEnter choice in Integer!! \n\n";
+            break;
+          }
+        } while (ch1 != 3);
+
+        return 0;
+      }
